978a78d1da7fd3046a71eff418cd22869294538c
Sasha Staryk <alexandrastryk@gmail.com>
2024-10-26 19:07:45 +0300

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const inisialCard = [
+const initialCards = [
   {
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,4 +25,4 @@ const inisialCard = [
   },
 ];
 
-console.log(inisialCard);
+console.log(initialCards);
